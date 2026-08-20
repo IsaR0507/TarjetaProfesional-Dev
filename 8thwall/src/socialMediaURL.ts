a0057375faac04901e46a1f5b043391a29fd9d69
IsaR0507 <urramirou0507@gmail.com>
2026-08-19 23:41:10 -0500

    TodoListoFinalDev

--- 8thwall/src/socialMediaURL.ts
+++ 8thwall/src/socialMediaURL.ts
@@ -2,17 +2,22 @@ import * as ecs from '@8thwall/ecs'
 
 ecs.registerComponent({
   name: 'linkButton',
+
   schema: {
-    button: ecs.eid,
     url: ecs.string,
   },
+
   stateMachine: ({world, eid, schemaAttribute}) => {
-    const {button, url} = schemaAttribute.get(eid)
+    const {url} = schemaAttribute.get(eid)
 
     ecs.defineState('default')
       .initial()
-      .listen(button, ecs.input.UI_CLICK, () => {
-        window.open(url, '_blank', 'noopener,noreferrer')
+      .listen(eid, ecs.input.SCREEN_TOUCH_START, () => {
+        console.log('Icono tocado:', eid)
+
+        if (url) {
+          window.open(url, '_blank')
+        }
       })
   },
 })
